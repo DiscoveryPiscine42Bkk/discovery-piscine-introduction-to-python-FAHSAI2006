fbd5e3cf7c4f1012b43f64a4a8028dfe91470c1b
FAHSAI2006 <68040582@kmitl.ac.th>
2025-07-19 05:33:42 +0000

    42.py cell00/ex01

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Aisaraporn"
+last_name = "Suksan"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Aisaraporn"
+last_name = "Suksan"
+whole_name = first_name + " " + last_name
+print(whole_name)
